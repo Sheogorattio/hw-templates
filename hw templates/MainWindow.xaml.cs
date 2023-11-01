689b89e74356640f633c1a1f16d781c7aa5e1594
Sheogorattio <91422100+Sheogorattio@users.noreply.github.com>
2023-11-01 07:33:32 +0200

    Add project files.

---
+++ hw templates/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace hw_templates
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Username:" + Usern.Text + "\nPassword: " + Passwd.Text + "\nIs checked: " + Chk.IsChecked);
+        }
+
+        private void CheckBox_Click(object sender, RoutedEventArgs e)
+        {
+            //if(Chk.IsChecked == true) Chk.IsChecked = false;
+            //else Chk.IsChecked=true;
+        }
+    }
+}
